4b11f7d1078750a31586a47ff1767d426aaf367f
Anil Raj Thota <anilrajthota@MacBook-Pro.local>
2021-08-06 07:06:32 +0530

    Update 7.prefix_evaluation.cpp

--- 3.Stack/7.prefix_evaluation.cpp
+++ 3.Stack/7.prefix_evaluation.cpp
@@ -4,5 +4,40 @@ using namespace std;
 int main()
 {
 
+    string str = "-+*23*549";
+
+    stack<int> s;
+
+    for (int i = str.size() - 1; i >= 0; i--)
+    {
+        if (int(str[i]) >= 48 && int(str[i]) <= 57)
+            s.push(str[i] - '0');
+        else
+        {
+            int val1 = s.top();
+            s.pop();
+            int val2 = s.top();
+            s.pop();
+
+            switch (str[i])
+            {
+            case '*':
+                s.push(val1 * val2);
+                break;
+            case '+':
+                s.push(val1 + val2);
+                break;
+            case '-':
+                s.push(val1 - val2);
+                break;
+            case '/':
+                s.push(val1 / val2);
+                break;
+            }
+        }
+    }
+
+    cout << s.top() << endl;
+
     return 0;
 }
